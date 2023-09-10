3813e24d591fd918878cf5dfe0feec44e0e211c0
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-09-10 16:33:29 +0500

    1

--- ex21/Program.cs
+++ ex21/Program.cs
@@ -10,7 +10,49 @@ namespace ex21
     {
         static void Main(string[] args)
         {
+            const string CommandSum = "sum";
+            const string CommandExit = "exit";
 
+            int[] array = new int[0];
+            bool isOpen = true;
+
+            while (isOpen)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("sum - суммировать введенные числа\nexit - выйти из программы\n");
+                Console.Write("Введите команду или число: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                int[] tempArray = new int[array.Length + 1];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    tempArray[i] = array[i];
+                }
+
+                tempArray[tempArray.Length - 1] = number;
+                array = tempArray;
+                Console.Clear();
+                Console.SetCursorPosition(0, 10);
+                Console.WriteLine("Введенные числа:");
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+            //switch ()
+            //{
+            //    case CommandSum:
+            //        break;
+
+            //    case CommandExit:
+            //        isOpen = false;
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Неверная команда.");
+            //        break;
+            //}
         }
     }
 }
